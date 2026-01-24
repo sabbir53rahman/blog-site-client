8b761f99d53a6e7e2accdc7cc63e77d6c6fc6e8d
sabbir53rahman <sabbir53rahman@gmail.com>
2026-01-24 22:02:14 +0600

    implement private route using proxy.ts

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,9 +1,41 @@
 import { NextRequest, NextResponse } from "next/server";
+import { userService } from "./services/user.service";
+import { Roles } from "./constants/roles";
 
-export function proxy(request: NextRequest) {
+export async function proxy(request: NextRequest) {
+  const pathName = request.nextUrl.pathname;
+  let isAuthenticated = false;
+  let isAdmin = false;
+
+  const { data } = await userService.getSession();
+
+  if (data) {
+    isAuthenticated = true;
+    isAdmin = data.user.role === Roles.admin;
+  }
+
+  //user is not authenticated
+  if (!isAuthenticated) {
+    return NextResponse.redirect(new URL("/auth/login", request.url));
+  }
+
+  //   user is authenticated but role is admin  and admin cant access user dashboard
+  if (isAdmin && pathName.startsWith("/dashboard")) {
+    return NextResponse.redirect(new URL("/admin-dashboard", request.url));
+  }
+
+  //   user is  authenticated but role is user and user cant access admin dashboard
+  if (!isAdmin && pathName.startsWith("/admin-dashboard")) {
+    return NextResponse.redirect(new URL("/dashboard", request.url));
+  }
   return NextResponse.next();
 }
 
 export const config = {
-  matcher: ["/dashboard"],
+  matcher: [
+    "/dashboard",
+    "/dashboard/:path*",
+    "/admin-dashboard",
+    "/admin-dashboard/:path*",
+  ],
 };
