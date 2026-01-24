46c5ce41025f84827547521a0f3ae844fcfc6030
sabbir53rahman <sabbir53rahman@gmail.com>
2026-01-24 08:09:54 +0600

    video 1 complete

--- src/app/(commonLayout)/page.tsx
+++ src/app/(commonLayout)/page.tsx
@@ -1,7 +1,18 @@
-import { authClient } from "@/lib/auth-client";
+import { cookies } from "next/headers";
 
 export default async function Home() {
-  const session = await authClient.getSession();
+  const cookieStore = await cookies();
+  console.log(cookieStore);
+
+  const res = await fetch("http://localhost:5000/api/auth/get-session", {
+    headers: {
+      Cookie: cookieStore.toString(),
+    },
+    cache: "no-store",
+  });
+
+  const session = await res.json();
+
   console.log(session);
   return <div className="">Home page</div>;
 }
